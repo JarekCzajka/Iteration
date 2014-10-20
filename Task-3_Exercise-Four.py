4951f7b06c62e93a963a52cac1102793cac083b7
Jaroslaw Czajka <40681@longroad.ac.uk>
2014-10-20 10:15:27 +0100

    Completed Exercise

---
+++ Task-3_Exercise-Four.py
@@ -0,0 +1,12 @@
+#Jaroslaw Czajka
+#20-10-2014
+#Task_6-Exercise-1 
+
+#Write a program that will ask the user for a message and the number of times they want
+#that message displayed. Then output the message that number of times.
+
+message=input("Please enter the message you want to display:")
+counter=int(input("How many times would you like for the message to be repeated?:"))
+for counter in range(counter):
+      print(message)
+#The program caried out the task succesfuly 
